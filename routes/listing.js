51182dfb1ed25fe7da6a420f21cbfeac0ac92a3e
sai111998 <61080910+sai111998@users.noreply.github.com>
2025-06-08 23:27:15 -0700

    Fix: listing

--- routes/listing.js
+++ routes/listing.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV != "production") {
+  require('dotenv').config();
+}
 const express = require("express");
 const router = express.Router();
 const Listing = require("../models/listing.js");
